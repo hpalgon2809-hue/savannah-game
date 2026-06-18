6c1dad48dd8fc8b99c8af55d0e761fafb96926f9
hpalgon2809-hue <hpalgon2809@hernanruiz.com>
2026-06-18 11:40:30 +0200

    Initialize Three.js scene with camera and renderer

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("JS funcionando");
+let scene, camera, renderer;
+
+init();
+animate();
+
+function init(){
+
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x87ceeb);
+
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+  camera.position.z = 10;
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  const ground = new THREE.Mesh(
+    new THREE.PlaneGeometry(100,100),
+    new THREE.MeshBasicMaterial({color:0x3c8f3c})
+  );
+
+  ground.rotation.x = -Math.PI/2;
+  scene.add(ground);
+
+  document.getElementById("info").textContent = "Mundo cargado ✔";
+
+}
+
+function animate(){
+  requestAnimationFrame(animate);
+  renderer.render(scene,camera);
+}
